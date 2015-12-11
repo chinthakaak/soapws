93795d870d1cb81239052a909b615e4803776513
chinthakaak <chinthakaak@gmail.com>
2015-12-11 16:44:56 +0530

    publishing endpoint on ip instead of localhost

--- src/main/java/com/examples/HelloWorldPublisher.java
+++ src/main/java/com/examples/HelloWorldPublisher.java
@@ -6,7 +6,7 @@ import javax.xml.ws.Endpoint;
 public class HelloWorldPublisher{
 
     public static void main(String[] args) {
-        Endpoint.publish("http://localhost:9999/ws/hello", new HelloWorldImpl());
+        Endpoint.publish("http://0.0.0.0:9999/ws/hello", new HelloWorldImpl());
     }
 }
 
